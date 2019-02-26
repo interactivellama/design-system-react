e96fba0e34c7f2a21f1c47eb0e3eaac12eb1b5f7
Stephen James <sjames@salesforce.com>
2019-02-26 14:52:37 -0700

    Visual Picker hack

--- components/input/__examples__/error.jsx
+++ components/input/__examples__/error.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import IconSettings from '~/components/icon-settings';
 import Input from '~/components/input'; // `~` is replaced with design-system-react at runtime
 import InputIcon from '~/components/icon/input-icon'; // `~` is replaced with design-system-react at runtime
+import Icon from '~/components/icon'; // `~` is replaced with
 
 class Example extends React.Component {
 	static displayName = 'ErrorInputExample';
@@ -11,37 +12,76 @@ class Example extends React.Component {
 		return (
 			<IconSettings iconPath="/assets/icons">
 				<div className="slds-grid slds-grid_pull-padded slds-grid_vertical-align-center">
-					<div className="slds-col_padded">
-						<Input
-							aria-describedby="error-4"
-							id="unique-id-4"
-							label="Input Label"
-							required
-							errorText="Error Message"
-							placeholder="Placeholder Text"
-						/>
-					</div>
-					<div className="slds-col_padded">
-						<Input
-							iconLeft={
-								<InputIcon
-									assistiveText={{ icon: 'Search' }}
-									name="warning"
-									category="utility"
-									color="warning"
-									onClick={() => {
-										console.log('Icon Clicked');
-									}}
+					<fieldset className="slds-form-element">
+						<legend className="slds-form-element__legend slds-form-element__label">
+							Select an option
+						</legend>
+						<ul className="slds-form-element__control">
+							<li className="slds-visual-picker slds-visual-picker_vertical">
+								<input
+									type="radio"
+									id="visual-picker-5"
+									name="options"
+									defaultValue="visual-picker-5"
 								/>
-							}
-							aria-describedby="error-5"
-							id="unique-id-5"
-							label="Input Label"
-							required
-							errorText="Error Message"
-							placeholder="Placeholder Text"
-						/>
-					</div>
+
+								<span className="slds-visual-picker__figure slds-visual-picker__text slds-align_absolute-center">
+									<span>
+										<label htmlFor="visual-picker-5">
+											<span className="slds-text-heading_medium slds-m-bottom_x-small">
+												Select this item
+											</span>
+											<span className="slds-text-title">
+												Some optional item description to help the user better
+												understand what this option is about.
+											</span>
+										</label>
+									</span>
+								</span>
+								<span className="slds-icon_container slds-visual-picker__text-check">
+									<svg
+										className="slds-icon slds-icon-text-check slds-icon_x-small"
+										aria-hidden="true"
+									>
+										<use
+											xmlnsXlink="http://www.w3.org/1999/xlink"
+											xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#check"
+										/>
+									</svg>
+								</span>
+							</li>
+							<li className="slds-visual-picker slds-visual-picker_vertical">
+								<input
+									type="radio"
+									id="visual-picker-6"
+									name="options"
+									defaultValue="visual-picker-6"
+								/>
+								<label htmlFor="visual-picker-6">
+									<span className="slds-visual-picker__figure slds-visual-picker__text slds-align_absolute-center">
+										<span>
+											<span className="slds-text-heading_medium slds-m-bottom_x-small">
+												Select this item
+											</span>
+											<span className="slds-text-title">
+												Some optional item description to help the user better
+												understand what this option is about.
+											</span>
+										</span>
+									</span>
+									<Icon
+										containerClassName="slds-visual-picker__text-check slds-icon_container"
+										className="slds-icon-text-check"
+										assistiveText={{ label: 'Check' }}
+										category="utility"
+										inverse
+										name="check"
+										size="x-small"
+									/>
+								</label>
+							</li>
+						</ul>
+					</fieldset>
 				</div>
 			</IconSettings>
 		);
